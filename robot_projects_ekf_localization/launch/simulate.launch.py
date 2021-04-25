28a7796b1a997121807a3e49356151fae2113ace
nick <nickm@outofthisworld.net>
2021-04-24 19:46:52 -0700

    added teleop keyboard to launch

--- robot_projects_ekf_localization/launch/simulate.launch.py
+++ robot_projects_ekf_localization/launch/simulate.launch.py
@@ -8,22 +8,33 @@ def generate_launch_description():
 
     sim_node = Node(
         package="robot_projects_simulator",
-        executable="ekf_sim_world"
+        executable="ekf_sim_world",
+        output={"both": "log"}
     )
 
     ekf_node = Node(
         package="robot_projects_ekf_localization",
-        executable="ekf_localization"
+        executable="ekf_localization",
+        output={"both": "log"}
     )
 
     vis_node = Node(
         package="rviz2",
         executable="rviz2",
-        arguments=["-d", os.path.join(get_package_share_directory("robot_projects_ekf_localization"), "config", "ekf_sim_world.rviz")]
+        arguments=["-d", os.path.join(get_package_share_directory("robot_projects_ekf_localization"), "config", "ekf_sim_world.rviz")],
+        output={"both": "log"}
+    )
+
+    test_act_node = Node(
+        package="teleop_twist_keyboard",
+        executable="teleop_twist_keyboard",
+        output='screen',
+        prefix = 'xterm -e'
     )
 
     ld.add_action(sim_node)
     ld.add_action(ekf_node)
     ld.add_action(vis_node)
+    ld.add_action(test_act_node)
 
     return ld
